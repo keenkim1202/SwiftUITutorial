e481523d09872a9da26ae193729136edeec2cc82
KEEN <homurahomu@naver.com>
2021-12-07 16:02:44 +0900

    프로젝트 생성

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by KEEN on 2021/12/07.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
