d8da17637ec839a70175d75d10f1d40e013ba583
KEEN <homurahomu@naver.com>
2021-12-08 16:32:31 +0900

    Category 폴더 생성 및 navigationView 설정

---
+++ Landmarks/View/Categories/CategoryHome.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by KEEN on 2021/12/08.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+  var body: some View {
+    NavigationView {
+      Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        .navigationTitle("Featured")
+    }
+  }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+  static var previews: some View {
+    CategoryHome()
+  }
+}
