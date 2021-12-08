20492aa0e164674637d4719460df837413c06630
KEEN <homurahomu@naver.com>
2021-12-08 13:50:15 +0900

    VStack, HStack 연습

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -8,14 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  var body: some View {
+    VStack {
+      MapView()
+        .ignoresSafeArea(edges: .top)
+        .frame(height: 300)
+      
+      CircleImage()
+        .offset(y: -130)
+        .padding(.bottom, -130)
+      
+      
+      VStack(alignment: .leading) {
+        Text("Turtle Rock")
+          .padding()
+          .font(.title)
+          .foregroundColor(.green)
+        
+        HStack {
+          Text("Joshua Tree National Park")
+            .font(.subheadline)
+          Spacer()
+          Text("California")
+            .font(.subheadline)
+        }
+        
+        Divider()
+        
+        Text("About Turtle Rock")
+          .font(.title2)
+        Text("Descriptive text goes here.")
+      }
+      .padding()
+      Spacer()
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
