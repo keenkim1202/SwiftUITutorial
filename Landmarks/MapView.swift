2e11e5027d770f46298d688ec0e33824fa57dae0
KEEN <homurahomu@naver.com>
2021-12-08 13:45:16 +0900

    MapView 다루기

--- Landmarks/MapView.swift
+++ Landmarks/MapView.swift
@@ -6,10 +6,24 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct MapView: View {
+  // @State : 앱의 데이터의 진실성을 확립해주는 속성이다. 하나 이상의 뷰에서 수정이 가능하다. SwiftUI는 값에 의존하여 자동으로 뷰를 업데이트하고 저장한다.
+  @State private var region = MKCoordinateRegion(
+    center: CLLocationCoordinate2D(
+      latitude: 34.011_286,
+      longitude: -116.166_868
+    ),
+    span: MKCoordinateSpan(
+      latitudeDelta: 0.2,
+      longitudeDelta: 0.2
+    )
+  )
+  
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+      // 상태 변수의 접두에 $를 붙여줌으로써, 값이 나타내는 레퍼런스가 누구인지 binding을 전달한다. 사용자가 map과 상호작용을 할 때, map은 ui상에서 현재 보이는 부분과 일치하는 region값 정보를 업데이트한다.
+      Map(coordinateRegion: $region)
     }
 }
 
