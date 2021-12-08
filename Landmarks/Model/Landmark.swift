e006b7ed40ce9980607b981807f1ea1fe5096f19
KEEN <homurahomu@naver.com>
2021-12-08 16:35:29 +0900

    category listView 생성

--- Landmarks/Model/Landmark.swift
+++ Landmarks/Model/Landmark.swift
@@ -9,8 +9,6 @@ import Foundation
 import SwiftUI
 import CoreLocation
 
-// Codable : data file과 구조체 사이의 데이터 이동을 쉽게 해주기 위해 사용한다.
-// Codable의 Decodable 컴포넌트에 의존하여 file로부터 데이터를 읽어올 것.
 struct Landmark: Hashable, Codable, Identifiable {
   var id: Int
   var name: String
@@ -19,13 +17,19 @@ struct Landmark: Hashable, Codable, Identifiable {
   var description: String
   var isFavorite: Bool
   
-  // 이미지 이름으로부터 이미지를 불러올 수 있도록 선언.
+  var category: Category
+  
+  enum Category: String, CaseIterable, Codable {
+    case lakes = "Lakes"
+    case rivers = "Rivers"
+    case mountains = "Mountains"
+  }
+  
   private var imageName: String
   var image: Image {
     Image(imageName)
   }
   
-  // 좌표 정포에 대한 모델 선언
   private var coordinates: Coordinates
   
   var locationCoordinate: CLLocationCoordinate2D {
