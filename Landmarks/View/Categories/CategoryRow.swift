f3d2608d4f97722329fb51f1d36910fbb610b487
KEEN <homurahomu@naver.com>
2021-12-08 16:45:40 +0900

    list로 collectionView 만들기

---
+++ Landmarks/View/Categories/CategoryRow.swift
@@ -0,0 +1,42 @@
+//
+//  CategoryRow.swift
+//  Landmarks
+//
+//  Created by KEEN on 2021/12/08.
+//
+
+import SwiftUI
+
+struct CategoryRow: View {
+  var categoryName: String
+  var items: [Landmark]
+  
+    var body: some View {
+      VStack(alignment: .leading) {
+        Text(categoryName)
+          .font(.headline)
+          .padding(.leading, 15)
+          .padding(.top, 5)
+        
+        ScrollView(.horizontal, showsIndicators: false) {
+          HStack(alignment: .top, spacing: 0) {
+            ForEach(items) { landmark in
+              CategoryItem(landmark: landmark)
+            }
+          }
+        }
+        .frame(height: 185)
+      }
+    }
+}
+
+struct CategoryRowPreviews: PreviewProvider {
+  static var landmark = ModelData().landmarks
+  
+    static var previews: some View {
+      CategoryRow(
+        categoryName: landmark[0].category.rawValue,
+        items: Array(landmark.prefix(4))
+      )
+    }
+}
