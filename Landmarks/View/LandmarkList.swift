4f44f9e5be8c4476752c7333c0b42d69f6530903
KEEN <homurahomu@naver.com>
2021-12-08 15:06:08 +0900

    toggle 구현 - isFavorite에 따라 분류2

--- Landmarks/View/LandmarkList.swift
+++ Landmarks/View/LandmarkList.swift
@@ -8,27 +8,36 @@
 import SwiftUI
 
 struct LandmarkList: View {
+  @State private var showFavoriteOnly = false
+  
+  var filterdLandmarks: [Landmark] {
+    landmarks.filter { landmark in
+      (!showFavoriteOnly || landmark.isFavorite)
+    }
+  }
+  
   var body: some View {
     NavigationView {
-      List(landmarks) { landmark in
-        NavigationLink {
-          LandmarkDetail(landmark: landmark)
-        } label: {
-          LandmarkRow(landmark: landmark)
+      List {
+        Toggle(isOn: $showFavoriteOnly) {
+          Text("Favorite Only")
         }
+        
+        ForEach(filterdLandmarks) { landmark in
+          NavigationLink {
+            LandmarkDetail(landmark: landmark)
+          } label: {
+            LandmarkRow(landmark: landmark)
+          }
+        }
+        .navigationTitle("Landmarks")
       }
-      .listStyle(.plain)
-      .navigationTitle("Landmarks")
     }
   }
 }
 
 struct LandmarkList_Previews: PreviewProvider {
   static var previews: some View {
-    ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
-      LandmarkList()
-        .previewDevice(PreviewDevice(rawValue: deviceName))
-        .previewDisplayName(deviceName)
-    }
+    LandmarkList()
   }
 }
